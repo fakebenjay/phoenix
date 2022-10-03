1dadb8a8cdc760e97e45ee526f68cef5665055c4
fakebenjay <fakebenjay@gmail.com>
2022-10-03 18:02:02 -0400

    cluster

--- mainv5.js
+++ mainv5.js
@@ -21,8 +21,8 @@ const map = new mapboxgl.Map({
   zoom: zoomScale(window.innerWidth),
   projection: 'mercator',
   maxBounds: [
-    [-113, 33.25],
-    [-111.5, 34]
+    [-112.85, 33.25],
+    [-111.5, 33.9]
   ]
 });
 
@@ -33,11 +33,6 @@ const nav = new mapboxgl.NavigationControl({
 map.addControl(nav, "top-right");
 
 map.on('load', () => {
-  // map.setPaintProperty('place-labels', 'fill-color', '#faafee');
-  // map.setPaintProperty('settlement-minor-layer', 'fill-color', '#faafee');
-
-
-
   map.addSource("zips", {
     type: "geojson",
     data: 'phoenix-data-filter.json',
@@ -451,18 +446,136 @@ function makeGeoJSON(csvData) {
 
     map.on('load', function() {
 
-      map.addLayer({
-        'id': 'airports',
-        'type': 'symbol',
-        'source': {
-          'type': 'geojson',
-          'data': data
-        },
-        'layout': {
-          "icon-image": "marker-15"
-        },
-        'paint': {}
+      map.addSource('houses', {
+        type: 'geojson',
+        // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
+        // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
+        data: data,
+        cluster: true,
+        clusterMaxZoom: 14, // Max zoom to cluster points on
+        clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
       });
+
+      map.addLayer({
+        id: 'clusters',
+        type: 'circle',
+        source: 'houses',
+        filter: ['has', 'point_count'],
+        paint: {
+          // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
+          // with three steps to implement three types of circles:
+          //   * Blue, 20px circles when point count is less than 100
+          //   * Yellow, 30px circles when point count is between 100 and 750
+          //   * Pink, 40px circles when point count is greater than or equal to 750
+          'circle-color': [
+            'step',
+            ['get', 'point_count'],
+            '#51bbd6',
+            100,
+            '#f1f075',
+            750,
+            '#f28cb1'
+          ],
+          'circle-radius': [
+            'step',
+            ['get', 'point_count'],
+            10,
+            50,
+            15,
+            375,
+            20
+          ]
+        }
+      });
+
+      map.addLayer({
+        id: 'cluster-count',
+        type: 'symbol',
+        source: 'houses',
+        filter: ['has', 'point_count'],
+        layout: {
+          'text-field': '{point_count_abbreviated}',
+          'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+          'text-size': 12
+        }
+      });
+
+      map.addLayer({
+        'id': 'unclustered-point',
+        'type': 'circle',
+        'source': 'houses',
+        // 'layout': {
+        //   "icon-image": "marker-15"
+        // },
+        // 'paint': {}
+        filter: ['!', ['has', 'point_count']],
+        paint: {
+          'circle-color': '#6ba292',
+          'circle-radius': 5,
+          'circle-stroke-width': 1,
+          'circle-stroke-color': '#fff'
+        }
+      });
+
+      map.on('click', 'clusters', (e) => {
+        const features = map.queryRenderedFeatures(e.point, {
+          layers: ['clusters']
+        });
+        const clusterId = features[0].properties.cluster_id;
+        map.getSource('houses').getClusterExpansionZoom(
+          clusterId,
+          (err, zoom) => {
+            if (err) return;
+
+            map.easeTo({
+              center: features[0].geometry.coordinates,
+              zoom: zoom + 1
+            });
+          }
+        );
+      });
+
+      // When a click event occurs on a feature in
+      // the unclustered-point layer, open a popup at
+      // the location of the feature, with
+      // description HTML from its properties.
+      map.on('click', 'unclustered-point', (e) => {
+        const coordinates = e.features[0].geometry.coordinates.slice();
+        const addr = e.features[0].properties['SITE_ADDR'];
+        const city = e.features[0].properties['S_CITY'];
+        const zip = e.features[0].properties['S_ZIP'];
+        const owner = e.features[0].properties['O_NAME'];
+        const owner1 = e.features[0].properties['O_NAME_ONE'];
+        const owner2 = e.features[0].properties['O_NAME_TWO'];
+
+        // Ensure that if the map is zoomed out such that
+        // multiple copies of the feature are visible, the
+        // popup appears over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        new mapboxgl.Popup()
+          .setLngLat(coordinates)
+          .setHTML(
+            `${addr}
+            <br/>
+            ${city} AZ, ${zip}
+            <br/><br/>
+            ${owner}
+            ${!!owner1 && owner1 != owner ? `<br>` + owner1:''}
+            ${!!owner2 && owner2 != owner1 && owner2 != owner? `<br>` + owner2:''}`
+          )
+          .addTo(map);
+      });
+
+      map.on('mouseenter', 'clusters', () => {
+        map.getCanvas().style.cursor = 'pointer';
+      });
+      map.on('mouseleave', 'clusters', () => {
+        map.getCanvas().style.cursor = '';
+      });
+
     });
   });
 }
